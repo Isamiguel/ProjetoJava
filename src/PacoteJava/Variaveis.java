ca12cf315fdb9a9eea430ad38606216dfb8a3756
Isamiguel <goncalvesisabellamiguel@gmail.com>
2023-05-24 17:32:34 -0300

    ProjetoJava

---
+++ src/PacoteJava/Variaveis.java
@@ -0,0 +1,13 @@
+package PacoteJava;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		// Variaveis
+ int numero;
+ float abono = 1000f;
+ boolean resposta;
+ String salario = "10000,00";
+	}
+
+}
